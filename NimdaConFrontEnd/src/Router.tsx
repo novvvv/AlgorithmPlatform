97828eb51f65e0795e0376d5907b229a0dfe1352
nov <novslog@gmail.com>
2025-10-30 15:27:01 +0900

    Initialize project

---
+++ NimdaConFrontEnd/src/Router.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "@/domains/User/Login/Page";
+import SignUp from "@/domains/User/Register";
+import ProblemSubmitPage from "@/domains/Contest/Problem/ProblemSubmit"; // [DEBUG] 테스트용 코드
+import JudgingStatusPage from "@/domains/Contest/Problem/JudgingStatus";
+import ProblemsPage from "@/domains/Contest/Problem/Problems";
+import ProblemCreatePage from "@/domains/Contest/Problem/ProblemCreate";
+import AdminDashboard from "@/domains/admin/AdminDashboard.jsx";
+import ProblemDetail from "@/domains/Contest/Problem/ProblemDetail/ProblemDetail.jsx";
+import Home from "@/domains/Home";
+
+import ContestHome from "@/domains/Contest/Home";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/problems" element={<ProblemsPage />} />
+        <Route path="/problem-submit" element={<ProblemSubmitPage />} />
+        <Route path="/problem-create" element={<ProblemCreatePage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/problem/:id" element={<ProblemDetail />} />
+        <Route path="/judging-status" element={<JudgingStatusPage />} />
+
+        <Route path="/contest" element={<ContestHome />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ README.md
@@ -0,0 +1,11 @@
+# NimdaPage
+Nimda Security Web Page Team Project 
+
+## BackEnd API 
+### 채점 관련 API
+
+`GET /api/judge/submissions`  - 코드 제출 목록 조회 
+
+### 유저 관련 API 
+
+`GET /api/auth/register` - 회원가입 API 
